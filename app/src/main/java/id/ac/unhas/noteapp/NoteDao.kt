eeab477a75092e6627102bf18a80846984e8e24f
RivaldoTikuAliSulle <aldorivaldo190@gmail.com>
2021-05-26 22:59:55 +0800

    komponen room

---
+++ app/src/main/java/id/ac/unhas/noteapp/NoteDao.kt
@@ -0,0 +1,34 @@
+package id.ac.unhas.noteapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+    @Query("Select * from note")
+    fun getNotes(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+}
+
+annotation class Update
+
+annotation class Delete
+
+annotation class Insert(val onConflict: Any)
+
+annotation class Query(val value: String)
+
+annotation class Dao
+
+class OnConflictStrategy {
+
+}
+
